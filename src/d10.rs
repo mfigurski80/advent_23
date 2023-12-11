8a4e7c7141556af37bf47b2b20d42924b0571328
Mikolaj Figurski <miko.f80@gmail.com>
2023-12-10 20:45:58 -0500

    complete d10 pt1

--- src/d10.rs
+++ src/d10.rs
@@ -1,35 +1,72 @@
 use crate::io_utils;
+use std::collections::HashSet;
+use std::collections::VecDeque;
 
 pub fn run() {
-    let lines = io_utils::read_file_lines("inputs/d10-example.txt").unwrap();
+    let lines = io_utils::read_file_lines("inputs/d10.txt").unwrap();
+    let map2d: Vec<Vec<TILE>> = lines
+        .into_iter()
+        .map(|mut line| {
+            line.insert(0, '.');
+            line.push('.');
+            line
+        })
+        .map(|line| line.bytes().map(match_tile).collect())
+        .collect();
+    // println!("Map: {:?}", map2d);
+    let start_pos = find_tile(&map2d, TILE::START).unwrap();
+    println!("Start pos: {:?}", start_pos);
+    let mut all_edges: VecDeque<Point> = VecDeque::new();
+    all_edges.push_back(start_pos);
+    let mut seen_set: HashSet<(usize, usize)> = HashSet::new();
+    seen_set.insert(start_pos);
 
-    let mut frags = lines.into_iter().map(|l| parse_line_fragments(l.as_str()));
-    let start = frags.next().unwrap();
-    println!("{:?}", start);
-    let pipes = frags.fold(start, |mut acc, fragments| {
-        combine_fragment_lines(acc.clone(), fragments)
-    });
+    while let Some(point) = all_edges.pop_front() {
+        let tile = map2d[point.0][point.1];
+        // println!("Visiting {:?} {:?}", point, tile);
+        let edges = tile_edges(tile);
+        for edge in edges {
+            let next_point = match edge {
+                TILEEDGE::UP => (point.0 - 1, point.1),
+                TILEEDGE::DOWN => (point.0 + 1, point.1),
+                TILEEDGE::LEFT => (point.0, point.1 - 1),
+                TILEEDGE::RIGHT => (point.0, point.1 + 1),
+            };
+            if seen_set.contains(&next_point) {
+                continue;
+            }
+            if tile == TILE::START {
+                let next_tile = map2d[next_point.0][next_point.1];
+                let next_edges: Vec<TILEEDGE> = tile_edges(next_tile)
+                    .iter()
+                    .map(corresponding_edge)
+                    .collect();
+                if !next_edges.contains(&edge) {
+                    continue;
+                }
+            }
+            all_edges.push_front(next_point);
+            seen_set.insert(next_point);
+        }
+    }
+
+    println!("Seen set: {:?}", seen_set);
+    println!("Seen set size: {:?}", seen_set.len());
 }
 
-#[derive(Debug, Clone)]
-struct PipeFragment {
-    length: u32,
-    up_connects: Vec<u32>,
-    down_connects: Vec<u32>,
-    excuse_index: Option<u32>,
-}
+type Point = (usize, usize);
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 #[repr(u8)]
 enum TILE {
-    EMPTY = b'.',
-    VERTICAL = b'|',
-    HORIZONTAL = b'-',
-    BOTTOMLEFT = b'L',
-    BOTTOMRIGHT = b'J',
-    TOPLEFT = b'F',
-    TOPRIGHT = b'7',
-    START = b'S',
+    EMPTY = 0,
+    VERTICAL = 1,
+    HORIZONTAL = 2,
+    BOTTOMLEFT = 3,
+    BOTTOMRIGHT = 4,
+    TOPLEFT = 5,
+    TOPRIGHT = 6,
+    START = 7,
 }
 
 fn match_tile(c: u8) -> TILE {
@@ -46,75 +83,45 @@ fn match_tile(c: u8) -> TILE {
     }
 }
 
-use regex::Regex;
-
-fn parse_line_fragments(line: &str) -> Vec<PipeFragment> {
-    let re = Regex::new(r"([SLF][S\-]*[SJ7])|(\|)").unwrap();
-    let fragments: Vec<PipeFragment> = re
-        .find_iter(line)
-        .map(|m| {
-            let start = m.start() as u32;
-            let end = m.end() as u32;
-            let mut up_connects = Vec::new();
-            let mut down_connects = Vec::new();
-            let start_tile = match_tile(m.as_str().as_bytes()[0]);
-            let end_tile = match_tile(m.as_str().as_bytes()[m.as_str().len() - 1]);
-            if start_tile == TILE::VERTICAL {
-                // edge case vertical
-                return PipeFragment {
-                    length: 1,
-                    up_connects: vec![start],
-                    down_connects: vec![start],
-                    excuse_index: None,
-                };
-            }
-            let mut excuse_index = None;
-            if start_tile == TILE::BOTTOMLEFT {
-                up_connects.push(start);
-            } else if start_tile == TILE::TOPLEFT {
-                down_connects.push(start);
-            } else if start_tile == TILE::START {
-                up_connects.push(start);
-                down_connects.push(start);
-                excuse_index = Some(start);
-            }
-            if end_tile == TILE::BOTTOMRIGHT {
-                up_connects.push(end - 1);
-            } else if end_tile == TILE::TOPRIGHT {
-                down_connects.push(end - 1);
-            } else if end_tile == TILE::START {
-                up_connects.push(end - 1);
-                down_connects.push(end - 1);
-                excuse_index = Some(end - 1);
-            }
-            PipeFragment {
-                length: end - start,
-                up_connects,
-                down_connects,
-                excuse_index,
-            }
-        })
-        .collect();
-    fragments
+fn find_tile(map2d: &Vec<Vec<TILE>>, tile: TILE) -> Option<(usize, usize)> {
+    map2d.iter().enumerate().find_map(|(y, row)| {
+        row.iter()
+            .enumerate()
+            .find_map(|(x, t)| if *t == tile { Some((x, y)) } else { None })
+    })
 }
 
-use std::collections::HashMap;
+#[derive(Debug, PartialEq)]
+enum TILEEDGE {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
 
-fn combine_fragment_lines(top: Vec<PipeFragment>, bottom: Vec<PipeFragment>) -> Vec<PipeFragment> {
-    // given multiple fragments, combine
-    // ----[↓    ↓ ]-[↓    ↓ ]----
-    // -[↓  ↑]--[↑    ↑]--[↑  ↓]-
-    let top_down_connects: HashMap<u32, usize> = top
-        .iter()
-        .enumerate()
-        .flat_map(|(i, f)| f.down_connects.iter().map(move |c| (*c, i)))
-        .collect();
-    let bottom_up_connects: HashMap<u32, usize> = bottom
-        .iter()
-        .enumerate()
-        .flat_map(|(i, f)| f.up_connects.iter().map(move |c| (*c, i)))
-        .collect();
-    println!("DOWN: {:?}", top_down_connects);
-    println!("UP:   {:?}", bottom_up_connects);
-    bottom
+fn tile_edges(tile: TILE) -> Vec<TILEEDGE> {
+    match tile {
+        TILE::VERTICAL => vec![TILEEDGE::UP, TILEEDGE::DOWN],
+        TILE::HORIZONTAL => vec![TILEEDGE::LEFT, TILEEDGE::RIGHT],
+        TILE::BOTTOMLEFT => vec![TILEEDGE::UP, TILEEDGE::RIGHT],
+        TILE::BOTTOMRIGHT => vec![TILEEDGE::UP, TILEEDGE::LEFT],
+        TILE::TOPLEFT => vec![TILEEDGE::DOWN, TILEEDGE::RIGHT],
+        TILE::TOPRIGHT => vec![TILEEDGE::DOWN, TILEEDGE::LEFT],
+        TILE::START => vec![
+            TILEEDGE::UP,
+            TILEEDGE::DOWN,
+            TILEEDGE::LEFT,
+            TILEEDGE::RIGHT,
+        ],
+        _ => vec![],
+    }
+}
+
+fn corresponding_edge(edge: &TILEEDGE) -> TILEEDGE {
+    match edge {
+        TILEEDGE::UP => TILEEDGE::DOWN,
+        TILEEDGE::DOWN => TILEEDGE::UP,
+        TILEEDGE::LEFT => TILEEDGE::RIGHT,
+        TILEEDGE::RIGHT => TILEEDGE::LEFT,
+    }
 }
