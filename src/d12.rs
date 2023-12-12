9a12543b641725ba30912feac15b7e593c32cc71
Mikolaj Figurski <miko.f80@gmail.com>
2023-12-12 17:04:35 -0500

    finish d12 pt1

--- src/d12.rs
+++ src/d12.rs
@@ -1,82 +1,58 @@
 use crate::io_utils;
 
 pub fn run() {
-    let lines = io_utils::read_file_lines("inputs/d12-example.txt").unwrap();
-    lines.map(parse_line).for_each(|(records, groups)| {
-        println!("{:?} into {:?}", groups, records);
+    let lines = io_utils::read_file_lines("inputs/d12.txt").unwrap();
+    let mut valid_sum = 0;
+    lines.map(parse_line).for_each(|(record, runs)| {
+        // println!("\t{} to {:?}", record, runs);
+        let broken_count = record.chars().filter(|c| *c == '#').count();
+        let broken_need = runs.iter().sum::<usize>();
+        // println!("Missing {}", broken_need - broken_count);
+        let all_combs = get_all_flip_combinations(record, broken_need - broken_count);
+        let valid_combs = all_combs.filter(|rec| record_match_runs(rec, &runs));
+        let n_valid = valid_combs.count();
+        println!("Valid: {}", n_valid);
+        valid_sum += n_valid;
     });
+    println!("Sum: {}", valid_sum);
 }
 
-type ProblemState = (Vec<String>, Vec<usize>);
-
-use regex::Regex;
-fn parse_line(line: String) -> ProblemState {
+fn parse_line(line: String) -> (String, Vec<usize>) {
     let mut parts = line.split(" ");
-    let left = parts.next().unwrap();
-    let records_re = Regex::new(r"[?#]+").unwrap();
-    let records = records_re
-        .find_iter(left)
-        .map(|m| m.as_str().to_string())
-        .collect::<Vec<_>>();
+    let left = parts.next().unwrap().to_string();
+    // let records_re = Regex::new(r"[?#]+").unwrap();
     let group_sizes = parts
         .next()
         .unwrap()
         .split(",")
         .map(|s| s.parse::<usize>().unwrap())
         .collect::<Vec<_>>();
-    (records, group_sizes)
+    (left, group_sizes)
 }
 
-fn next_problem(state: ProblemState) -> Vec<ProblemState> {
-    let (records, group_sizes) = state;
-    if records.len() == 0 || group_sizes.len() == 0 {
-        return vec![];
-    }
-    let group_size = group_sizes[0];
-    let record = &records[0];
-    let record_len = record.len();
-    if group_size > record_len {
-        // return problem state wihout first record
-        return vec![(records[1..].to_vec(), group_sizes)];
-    }
-    for ch_i in 0..record_len - group_size {
-        // all possible permutations of this record in this position
-        let ch = record.chars().nth(ch_i).unwrap();
-        let new_record = record.clone()[ch_i..ch_i + group_size].to_string();
-        let leftover_record = match record_len - ch_i - group_size - 1 {
-            0 => "".to_string(),
-            _ => record.clone()[ch_i + group_size + 1..].to_string(),
-        };
-    }
+use regex::Regex;
 
-    vec![]
-}
-
-fn record_match_runs(record: Vec<String>, runs: Vec<usize>) -> bool {
-    return record.len() == runs.len()
-        && record
-            .iter()
-            .zip(runs.iter())
-            .all(|(r, run)| (*r).len() == *run);
-}
-
-fn get_unknown_indexes(record: &str) -> Vec<usize> {
-    record
-        .chars()
-        .filter(|c| *c == '?')
-        .map(|c| c as usize)
-        .collect()
+fn record_match_runs(record: &String, runs: &Vec<usize>) -> bool {
+    let record_runs = Regex::new("#+")
+        .unwrap()
+        .find_iter(&record)
+        .map(|m| m.len())
+        .collect::<Vec<usize>>();
+    record_runs.iter().zip(runs.iter()).all(|(a, b)| a == b)
 }
 
 use itertools::Itertools;
 
-fn get_all_flip_permutes(record: String, num_to_flip: usize) -> impl Iterator<Item = String> {
+fn get_all_flip_combinations(record: String, num_to_flip: usize) -> impl Iterator<Item = String> {
     let original_record = record.clone();
-    let unknown = get_unknown_indexes(&record);
+    let unknown = record
+        .chars()
+        .enumerate()
+        .filter(|(_, c)| *c == '?')
+        .map(|(i, _)| i)
+        .collect::<Vec<_>>();
     if unknown.len() < num_to_flip {
         panic!("Not enough unknowns to flip");
-    } else if unknown.len() == num_to_flip {
-        return vec![record].into_iter();
     }
     let it = unknown.iter().combinations(num_to_flip).map(|perm| {
         let mut rec = original_record.clone();
@@ -90,7 +66,7 @@ fn get_all_flip_permutes(record: String, num_to_flip: usize) -> impl Iterator<It
 }
 
 /*
- * Is there a way to turn this into a permutation problem?
+ * Is there a way to turn String + Run assignment into a permutation problem?
  *
  * We have: ??##??## and need to fit [3,2] into it...
  * Could be:.###..##
@@ -105,7 +81,68 @@ fn get_all_flip_permutes(record: String, num_to_flip: usize) -> impl Iterator<It
  * Example is also too easy, try where we have more # segments than runs
  * We have: ?#?#??#?# and need to fit [4,3] into it...
  * Can we just optimize by counting how many # we're missing?
- *   4,3 implies 7 #. We have 5, so we need to add 2 more
+ *   4,3 implies 7 #. We have 4 #, so we need to add 3 more
  *   Try every permutation of setting 2 ? into #, of which there are:
- *   (5 choose 2) = 10. Not bad. This example isn't too far off from the real thing
+ *   (5 choose 3) = 10. Not bad. This example isn't too far off from the real thing
+ *   ?#?#??#?#
+ *   #####.#.# => doesnt match [4,3]
+ *   ####.##.# => no
+ *   ####..### => yes!
+ *   ##.####.# => no
+ *   ##.##.### => no
+ *   ##.#.#### => no
+ *   .####.### => yes!
+ *   .###.#### => no
+ *   Also this is a 'combination' problem instead -- order doesn't matter
+ */
+
+/* How do we determine how to assign runs to records? Permutation problem?
+ * We have Records: [a,b,c] and Runs we need to distribute (or not) [2,3,4]...
+ * Could be: [2,3,4], [23,4, ], [2,34, ], [2, ,34], [23, ,4], [ ,23,4], [ ,2,34], [234, , ], [ ,234, ], [ , ,234]
+ * 10 in total
+ * Can we represent each option as a diff? Each run needs from 1..3 records assigned to it
+ *   Into: [1,1,1], [2,1,0], [1,2,0], [1,0,2], [2,0,1], [0,2,1], [0,1,2], [3,0,0], [0,3,0], [0,0,3]
+ *   Still not very permutation-like
+ * Can we represent each option as assigning record to run?
+ *   Into: [a,b,c], [b,c, ], [a,c, ], [a, ,c], [b, ,a], [ ,b,a], [ ,a,c], [c, , ], [ ,c, ], [ , ,c]
+ *   Ehh
+ * Can we represent this with recursion to a simple base case? Ie [a,b] distribute [2,3]
+ *   base(2,3): [2,3], [23, ], [ ,23]
+ *   Larger problem can be grouped:
+ *      [base(2,3),4] => [2,3,4] [23, ,4] [ ,23,4]
+ *      [2,base(3,4)] => [2,3,4] [2,34, ] [2, ,34]
+ *      [base(23,4),] => [23,4,] [234, ,] [ ,234,]
+ *      [,base(23,4)] => [,23,4] [,234, ] [, ,234]
+ *      [base(2,34),] => [2,34,] [234, ,] [ ,234,]
+ *      [,base(2,34)] => [,2,34] [,234, ] [, ,234]
+ * Can we simply have separate problem classes for each run?
+ *    [0] => [1,1,1]
+ *    [1] => [2,1,0], [1,2,0], [1,0,2]
+ *    [2] => [2,0,1], [0,2,1], [0,1,2]
+ * Should we just optimize with |Runs| for-loops?
+ *   Where we can test [i,j,k] (diff, example 1)...
+ *   for i in 0..|Runs|
+ *     for j in i..|Runs|
+ *       for k in j..|Runs|
+ *         now i + j + k === |Runs|!!
+ *         Preconditions for #1 are valid. Each index takes from 1..3
+ * Maybe we should just use the `get_all_flip_permutes` function with everything?
+ *   We have a giant string: .??..??...?## and need to fit Runs [1,1,3]
+ *   We need 5 #s, have only 2: need 3 flips.
+ *   Indexes of '?' are [1,2,5,6,10]. Every combination of 3 (5 choose 3 is 10):
+ *     [1,2,5], [1,2,6], [1,2,10], [1,5,6], [1,5,10], [1,6,10], [2,5,6], [2,5,10], [2,6,10], [5,6,10]
+ *     .??..??...?##
+ *     .##..#.....## => doesnt match [1,1,3]
+ *     .##...#....## => no
+ *     .##.......### => no
+ *     .#...##....## => no
+ *     .#...#....### => yes!
+ *     .#....#...### => yes!
+ *     ..#..##....## => no
+ *     ..#..#....### => yes!
+ *     ..#...#...### => yes!
+ *     .....###..### => no
+ *   This is totally reasonable. Fuck all this permutation stuff, just operate on single line
+ *   Might be hard to memoize though => not really being reduced into repeating subproblems well
+ *
  */
