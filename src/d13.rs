79fe26324a4b8a5af14b834db82e2801a7885743
Mikolaj Figurski <miko.f80@gmail.com>
2023-12-13 21:56:58 -0500

    do d13 pt2

--- src/d13.rs
+++ src/d13.rs
@@ -12,7 +12,7 @@ pub fn run() {
         let fold_row_index = find_fold_index(row_hashes);
         if let Some(fold_index) = fold_row_index {
             println!("fold_index: ROW {}", fold_index);
-            row_matches += fold_index + 1;
+            row_matches += fold_index;
             continue;
         }
         let transposed = transpose_map(
@@ -30,7 +30,7 @@ pub fn run() {
         let fold_col_index = find_fold_index(col_hashes);
         if let Some(fold_index) = fold_col_index {
             println!("fold_index: COL {}", fold_index);
-            col_matches += fold_index + 1;
+            col_matches += fold_index;
         }
     }
     println!("col_matches: {}", col_matches);
@@ -48,12 +48,24 @@ fn hash_row(row: String) -> u64 {
     hash
 }
 
+fn hash_row_dif(a: u64, b: u64) -> u32 {
+    (a ^ b).count_ones()
+}
+
 fn find_fold_index(row_hashes: Vec<u64>) -> Option<usize> {
     for i in 1..(row_hashes.len()) {
         let (left, right) = row_hashes.split_at(i);
-        let valid = left.iter().rev().zip(right.iter()).all(|(l, r)| l == r);
-        if valid {
-            return Some(i - 1);
+        let mut used_smudge = false;
+        let valid = left.iter().rev().zip(right.iter()).all(|(l, r)| {
+            let dif = hash_row_dif(*l, *r);
+            if dif == 1 && !used_smudge {
+                used_smudge = true;
+                return true;
+            }
+            return dif == 0;
+        });
+        if valid && used_smudge {
+            return Some(i);
         }
     }
     None
