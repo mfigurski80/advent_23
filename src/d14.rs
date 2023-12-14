cc79610d8bf8604a2f27b637b9fad8f0497d99b0
Mikolaj Figurski <miko.f80@gmail.com>
2023-12-14 12:54:18 -0500

    do d14 pt1

--- src/d14.rs
+++ src/d14.rs
@@ -2,15 +2,43 @@ use crate::io_utils;
 use crate::map_utils;
 
 pub fn run() {
-    let lines = io_utils::read_file_lines("inputs/d14-example.txt").unwrap();
+    let lines = io_utils::read_file_lines("inputs/d14.txt").unwrap();
     let map = lines.collect::<map_utils::Map>();
-    // map.iter().for_each(|r| println!("{}", r));
-    let map_t = map_utils::transpose(map);
-    let tilted = map_t
+    map.iter().for_each(|r| println!("{}", r));
+    let map_t = map_utils::rotate(map);
+    let tilted_t = map_t
         .iter()
-        .map(|r| {
-            println!("{}", r);
-            r.to_owned()
-        })
+        .map(|r| tilt_row(r.to_string()))
         .collect::<map_utils::Map>();
+    let tilted = map_utils::rotate(tilted_t);
+    tilted.iter().for_each(|r| println!("{}", r));
+    println!("score: {}", score_map(tilted));
+}
+
+fn tilt_row(row: String) -> String {
+    let sections = row.split('#').map(|s| {
+        let roll_count = s.chars().filter(|c| *c == 'O').count();
+        let mut section = String::new();
+        for _ in 0..roll_count {
+            section.push('O');
+        }
+        for _ in roll_count..s.len() {
+            section.push('.');
+        }
+        section + "#"
+    });
+    // remove last '#'
+    sections.collect::<String>()[..row.len()].to_string()
+}
+
+fn score_map(map: map_utils::Map) -> u32 {
+    let counts = map
+        .iter()
+        .map(|r| r.chars().filter(|c| *c == 'O').count() as u32);
+    let vals = counts
+        .rev()
+        .enumerate()
+        .map(|(i, count)| count * (i + 1) as u32);
+    // println!("{:?}", vals.collect::<Vec<u32>>());
+    vals.sum()
 }
--- src/map_utils.rs
+++ src/map_utils.rs
@@ -1,13 +1,13 @@
 pub type Map = Vec<String>;
 
-pub fn transpose(map: Map) -> Map {
-    let mut transposed = Vec::new();
+pub fn rotate(map: Map) -> Map {
+    let mut rotated = Vec::new();
     for i in 0..map[0].len() {
         let mut row = String::new();
         for j in 0..map.len() {
             row.push(map[j].as_bytes()[i] as char);
         }
-        transposed.push(row);
+        rotated.push(row);
     }
-    transposed
+    rotated
 }
